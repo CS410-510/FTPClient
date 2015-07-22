6fe27ee9a875a02d436c41a8ac0ec1f4ceb0a372
Ryan Mac <ryanandersont@gmail.com>
2015-07-22 16:12:16 -0700

    put files

--- src/main/java/FTPDispatcher.java
+++ src/main/java/FTPDispatcher.java
@@ -27,18 +27,22 @@ public class FTPDispatcher {
 
             if (line.hasOption('C') || line.hasOption("connect")) {
                 // handle server connection option
-
                 commands.connect(ftp, line.getOptionValue('C'));
             }
 
             if (line.hasOption("l") || line.hasOption("list")) {
                 // handle option to list files
-
                 commands.listFilesFolders(ftp);
             }
 
             if (line.hasOption('g') || line.hasOption("get")) {
+                // handle getting a file on remote
                 commands.getRemoteFile(ftp, line.getOptionValue('g'));
+            }
+
+            if (line.hasOption('p') || line.hasOption("put")) {
+                // handle putting a file on remote
+                commands.putRemoteFile(ftp,line.getOptionValue('p'));
             }
 
             commands.exit(ftp);
