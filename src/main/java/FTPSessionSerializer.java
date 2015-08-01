c84637544505d4467f0b208dadb82bd5302ba349
Sergio Garcia <sgarciapdx@gmail.com>
2015-08-01 12:55:32 -0700

    Added file args to serializer, added tests, reworked tests to not use default save file

--- src/main/java/FTPSessionSerializer.java
+++ src/main/java/FTPSessionSerializer.java
@@ -10,10 +10,13 @@ import java.io.IOException;
  * @author Sergio Garcia
  */
 public class FTPSessionSerializer {
+    // This is used for creating a default save file in the System's current
+    // directory (typically the directory where the project is executed).
     static final String SESSION_FILENAME = ".savedsession";
 
     /**
-     * This handles the deserialization of a FTPSession. If an issue is encountered
+     * This handles the deserialization of a FTPSession and is a convenience
+     * wrapper for loading from a default file. If an issue is encountered
      * during deserialization, this will return null.
      *
      * @return a restored FTPSession
@@ -21,9 +24,22 @@ public class FTPSessionSerializer {
      * @throws ClassNotFoundException if an error is encountered during deserialization
      */
     public static FTPSession deserialize() throws IOException, ClassNotFoundException {
+        return deserialize(SESSION_FILENAME);
+    }
+
+    /**
+     * This handles the deserialization of a FTPSession from a specific file. If
+     * an issue is encountered during deserialization, this will return null.
+     *
+     * @param filepath absolute path of file to deserialize from
+     * @return a restored FTPSession
+     * @throws IOException if an error is encountered during reading the stream
+     * @throws ClassNotFoundException if an error is encountered during deserialization
+     */
+    public static FTPSession deserialize(String filepath) throws IOException, ClassNotFoundException {
         FTPSession session;
 
-        try (ObjectInputStream filein = new ObjectInputStream(new FileInputStream(SESSION_FILENAME))) {
+        try (ObjectInputStream filein = new ObjectInputStream(new FileInputStream(filepath))) {
             session = (FTPSession) filein.readObject();
             session.restore();
         }
@@ -32,13 +48,25 @@ public class FTPSessionSerializer {
     }
 
     /**
-     * This handles the serialization of a FTPSession.
+     * This handles the serialization of a FTPSession and is a convenience wrapper
+     * for saving to a default file.
      *
      * @param session an FTPSession
      * @throws IOException if an error is encountered during serialization
      */
     public static void serialize(FTPSession session) throws IOException {
-        try (ObjectOutputStream fileout = new ObjectOutputStream(new FileOutputStream(SESSION_FILENAME))) {
+        serialize(session, SESSION_FILENAME);
+    }
+
+    /**
+     * This handles the serialization of a FTPSession to a provided file.
+     *
+     * @param session an FTPSession
+     * @param filepath absolute path of file to serialize to
+     * @throws IOException if an error is encountered during serialization
+     */
+    public static void serialize(FTPSession session, String filepath) throws IOException {
+        try (ObjectOutputStream fileout = new ObjectOutputStream(new FileOutputStream(filepath))) {
             fileout.writeObject(session);
         }
     }
--- src/test/java/FTPSessionSerializerTest.java
+++ src/test/java/FTPSessionSerializerTest.java
@@ -1,15 +1,14 @@
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
+import org.junit.rules.TemporaryFolder;
 
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Tests for FTPSessionSerializer
@@ -17,55 +16,78 @@ import java.io.IOException;
 @RunWith(Enclosed.class)
 public class FTPSessionSerializerTest {
 
-    @FixMethodOrder (MethodSorters.NAME_ASCENDING)
+    @FixMethodOrder(MethodSorters.NAME_ASCENDING)
     public static class goodSerialization {
+        @ClassRule
+        public static TemporaryFolder folder = new TemporaryFolder();
+
         private static FTPSession originalSession, restoredSession;
         private static final String HOST = "52.25.152.38";
         private static final int PORT = 21;
         private static final String USER = "ftptestuser";
         private static final String PASSWORD = "password";
-        private static File savedSession;
-        private static String newDir = "dirForFTPSerializerTest";
+        private static File newDir;
+        private static File savedSessionFile;
 
         @BeforeClass
         public static void setup() throws Exception {
             originalSession = new FTPSession();
             originalSession.connect(HOST, PORT);
             originalSession.login(USER, PASSWORD);
-            savedSession = new File(FTPSessionSerializer.SESSION_FILENAME);
+            savedSessionFile = folder.newFile();
+            newDir = folder.newFolder();
         }
 
         @AfterClass
         public static void teardown() throws Exception {
             originalSession.disconnect();
-            savedSession.delete();
         }
 
         @Test
         public void test01CanSerializeSession() throws Exception {
-            assertTrue(originalSession.isConnected());
-            File testDir = originalSession.createLocalFile(newDir);
-            testDir.mkdir();
-            originalSession.setLocalDir(testDir.getPath());
-            FTPSessionSerializer.serialize(originalSession);
-            assertTrue(savedSession.exists());
+            assertTrue("session is not connected", originalSession.isConnected());
+            // Adding a directory to change session's local directory.
+            newDir.mkdir();
+            originalSession.setLocalDir(newDir.getPath());
+            FTPSessionSerializer.serialize(originalSession, savedSessionFile.getAbsolutePath());
+            assertTrue("serialization file doesn't exist", savedSessionFile.exists());
         }
 
         @Test
         public void test02CanDeserializeSession() throws Exception {
-            restoredSession = FTPSessionSerializer.deserialize();
-            File testDir = new File(newDir);
-            testDir.deleteOnExit();
-            assertTrue(restoredSession.getCurrentLocalDir().equals(testDir.getAbsolutePath()));
-            assertTrue(restoredSession.isConnected());
+            restoredSession = FTPSessionSerializer.deserialize(savedSessionFile.getAbsolutePath());
+            assertTrue("restored session missing new directory",
+                    restoredSession.getCurrentLocalDir().equals(newDir.getAbsolutePath()));
+            assertTrue("restored session not connected", restoredSession.isConnected());
         }
     }
 
     @FixMethodOrder (MethodSorters.NAME_ASCENDING)
     public static class badSerialization {
         @Test (expected = IOException.class)
-        public void serializationWithNoFileThrowsException() throws Exception {
-            FTPSession session = FTPSessionSerializer.deserialize();
+        public void deserializationWithNoFileThrowsException() throws Exception {
+            FTPSession session = FTPSessionSerializer.deserialize("nonexistentfile");
         }
+
+        @Test (expected = IOException.class)
+        public void deserializationWithEmptyFileThrowsException() throws Exception {
+            File badsession = new File("emptyfile");
+            badsession.createNewFile();
+            badsession.deleteOnExit();
+            FTPSession session = FTPSessionSerializer.deserialize(badsession.getAbsolutePath());
+        }
+
+        @Test (expected = IOException.class)
+        public void deserializationWithBadFileThrowsException() throws Exception {
+            File badsession = new File("badfile");
+            badsession.createNewFile();
+            badsession.deleteOnExit();
+            PrintWriter pw = new PrintWriter(badsession);
+            pw.println("FTPSessionSerializer is for nerds!");
+            pw.flush();
+            pw.close();
+            FTPSession session = FTPSessionSerializer.deserialize(badsession.getAbsolutePath());
+        }
+
     }
 }
